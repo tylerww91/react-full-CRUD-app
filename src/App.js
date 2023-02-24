21293d45fde01657da9ebc04876cac4a2e69090e
Tyler Watson <tylerww91@gmail.com>
2023-02-23 17:00:48 -0800

    builds out App, and establishes necessary routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Auth from './components/Auth/Auth.js';
+import Nav from './components/Nav/Nav.js';
+import EditPost from './components/Posts/EditPost.js';
+import NewPost from './components/Posts/NewPost.js';
+import PostList from './components/Posts/PostList.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Switch>
+        <Route path="/auth/:type" component={Auth} />
+        <Route path="/posts/edit/:id" component={EditPost} />
+        <Route path="/posts/new" component={NewPost} />
+        <Route path="/posts" component={PostList} />
+        <Route path="*">
+          <Redirect to="/auth/sign-in" />
+        </Route>
+      </Switch>
     </div>
   );
 }
