27e6ca6d324a942ed0ed19ccb6892174ed29767c
Tyler Watson <tylerww91@gmail.com>
2023-02-23 16:59:28 -0800

    builds createPost, getPostDetail, and updatePost

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,6 +1,21 @@
-import { checkError, client } from "./client.js";
+import { checkError, client } from './client.js';
 
 export async function getPosts() {
   const resp = await client.from('posts').select('*');
+  return checkError(resp);
+}
+
+export async function createPost(title, description) {
+  const resp = await client.from('posts').insert({ title, description });
+  return checkError(resp);
+}
+
+export async function getPostDetail(id) {
+  const resp = await client.from('posts').select('*').match({ id }).single();
+  return checkError(resp);
+}
+
+export async function updatePost(id, title, description) {
+  const resp = await client.from('posts').update({ title, description }).match({ id });
   return checkError(resp);
 }
